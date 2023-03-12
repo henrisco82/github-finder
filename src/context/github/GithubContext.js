b7718f80a1397002eb6ceb14353a06ea9203d52d
Henry Unah <Henry-Odohi.Unah@external.telekom.de>
2023-03-12 20:58:12 +0100

    final working version

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -3,30 +3,21 @@ import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext()
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
-
 export const GithubProvider = ({children}) => {
     const initialState = {
         users: [],
-        loading: true
+        user: {},
+        repos: [],
+        loading: false
     }
-
     const [state, dispatch] = useReducer(githubReducer, initialState)
-    const fetchUsers = async () => {
-        const response = await fetch(`${GITHUB_URL}/users`, {
-          headers: {
-            Authorization: `token ${GITHUB_TOKEN}`
-          }
-        })
-        const data = await response.json()
-        dispatch(
-            type: 
-        )
-        setLoading(false)
-    }
 
-    return <GithubContext.Provider value={{users, loading, fetchUsers}}>
+    return <GithubContext.Provider 
+    value={{
+        ...state,
+        dispatch,
+        }}
+        >
         {children}
     </GithubContext.Provider>
 }
